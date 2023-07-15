8cfbdd64b67db1ffd23f68332fdaa9e316e472d7
MR. U7P4L 1N <123867620+U7P4L-IN@users.noreply.github.com>
2023-07-15 14:27:25 +0600

    Update Banner.py

--- Banner.py
+++ Banner.py
@@ -23,7 +23,7 @@ import os,sys,time,random
 print("")
 print("")
 color = ["\\033[1;31m","\\033[1;32m"]
-m = random.choice(color)+"Welcome {} \\n"
+m = random.choice(color)+"WELCOME {} \\n"
 for msg in m:
     sys.stdout.write(msg)
     sys.stdout.flush()
